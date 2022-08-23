b3c1f382a9ffd29584ba150a1f0e92e0fc4e51bd
Anukul Nepal <anukulnepal2014@gmail.com>
2022-08-23 15:09:26 +0545

    Javascr

---
+++ script.js
@@ -0,0 +1,8 @@
+let js = 'amazing';
+console.log(40+10+23-10);
+console.log("Acool");
+console.log(23);
+let firstName = "Acool";
+let firstName = "Acool";
+console.log(firstName);
+let jonas1Matilda ='JM';
---
+++ scriptVariables.js
@@ -0,0 +1,13 @@
+/*let country ="Nepal";
+let continent ="Asia";
+let population ="30Million";
+console.log(country);
+console.log(continent);
+console.log(population);*/
+
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+console.log(typeof true);
+console.log(typeof 23);
+console.log(typeof "Anukul");
+    
