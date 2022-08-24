fd350eb76392c633d9fa81a5855be72efa521253
Anukul Nepal <anukulnepal2014@gmail.com>
2022-08-24 12:26:29 +0545

    New

---
+++ challenge1.js
@@ -0,0 +1,20 @@
+/* let weightMarks = 78;
+let heightMarks = 1.69;
+let weightJohn = 92;
+let heightJohn = 1.95; */
+/* const bmiMarks = weightMarks / heightMarks ** 2;
+const bmiJohn = weightJohn / heightJohn ** 2 ; */
+/* const markHigherBMI = bmiMarks > bmiJohn ;
+console.log(markHigherBMI,bmiMarks, bmiJohn); */
+/* console.log(
+    (bmiMarks = weightMarks / heightMarks ** 2),
+    (bmiJohn = weightJohn / heightJohn ** 2),
+    (markHigherBMI = bmiJohn < bmiMarks));
+ */
+const firstName = "Jonas";
+const job = "teacher";
+const birthYear = 1991;
+// const jonas = "I'm " + firstName + ', a ' + (2037-birthYear) + " years old " + job + ' !'
+// console.log(jonas);
+const jonasNew = `I'm ${firstName}, a ${2037-birthYear} year old ${job}`;
+console.log(jonasNew);
--- scriptVariables.js
+++ scriptVariables.js
@@ -11,3 +11,46 @@ console.log(year);
 console.log(typeof year);
 year = 1991;
 console.log(typeof year); */
+
+/* let age = 30;
+age = 31;
+
+const birthYear = 1991;
+birthYear = 1990;
+ */
+
+// Math Operators :
+
+/* const birthYear = 2037;
+const ageJonas = birthYear - 1991;
+const ageSarah = birthYear - 2020;
+console.log(ageJonas, ageSarah);
+console.log(ageJonas * 2 , ageJonas / 10, 2**3);
+// 2**3 means 2 to the power of 3 = 2*2*2 = 8
+const firstName = "Jonas";
+const lastName =  "Schmedtmann";
+console.log(firstName + " " + lastName);
+
+// Assignment Operators:
+
+let x = 10 + 5 ; // 15
+x += 10; //x = x + 10 = 25
+x *= 4; //x = x * 4 = 100
+x++; // x = x + 1
+x --;
+x --;
+console.log(x);
+
+// Comparison Operators
+console.log(ageJonas > ageSarah); // < , > ,>= , <=
+console.log(ageSarah >=18);
+console.log(ageSarah <=18); */
+/* 
+const birthYear = 2037;
+const ageJonas = birthYear - 1991;
+const ageSarah = birthYear - 2018;
+
+console.log(birthYear - 1991 > birthYear - 2018);
+const averageAge = (ageJonas + ageSarah) / 2
+console.log (ageJonas, ageSarah, averageAge); */
+
